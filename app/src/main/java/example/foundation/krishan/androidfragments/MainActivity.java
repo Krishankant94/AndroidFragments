84401a58394e1ba5d670e621470de17b13d5df03
Krishan Kant <tom.george@metlife.com>
2017-11-19 22:39:49 +0530

    Initial commit

---
+++ app/src/main/java/example/foundation/krishan/androidfragments/MainActivity.java
@@ -0,0 +1,34 @@
+package example.foundation.krishan.androidfragments;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+        Fragment fr;
+
+        if (view == findViewById(R.id.button2)) {
+            fr = new FragmentTwo();
+
+        } else {
+            fr = new FragmentOne();
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+}
